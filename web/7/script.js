d21f14a5d37e5772ec3c94b129d222aa033edd4d
Robotzrz <robotzr167@gmail.com>
2024-03-11 21:27:04 +0300

    1

--- web/7/script.js
+++
@@ -1,6 +0,0 @@
-
-document.getElementById("square").addEventListener("click", () => {
-    document.getElementById("square").style.top = Math.random() * (1080 - 500) + 150 + "px";
-    document.getElementById("square").style.left = Math.random() * (1920 - 650) + 370 + "px";
-    document.getElementById("square").style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16);
-}); 
